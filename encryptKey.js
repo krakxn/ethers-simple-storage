f45e4294ad1beab25a08dd0b9ae5caf37121412e
AN <108299225+krakxn@users.noreply.github.com>
2022-06-30 05:09:22 +0530

    update

--- encryptKey.js
+++ encryptKey.js
@@ -2,14 +2,14 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
 
-require("dotenv").config();
+require("dotenv").config(); // To process .env file
 async function main() {
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
   const encryptedJsonKey = await wallet.encrypt(
     process.env.PRIVATE_KEY_PASSWORD,
     process.env.PRIVATE_KEY
   );
-  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey); // Stores encrypted key
 }
 main()
   .then(() => process.exit(0))
