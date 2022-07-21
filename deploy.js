add3b6578f67854937a70f3deb26053f4b070a06
AN <108299225+krakxn@users.noreply.github.com>
2022-07-21 23:57:47 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,9 +1,9 @@
-// Used to deploy contract
 const ethers = require("ethers")
 const fs = require("fs-extra")
 require("dotenv").config()
 async function main() {
     let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    // Following is the encryption
     /* const encryptedJson = fs.readFileSync("./.encryptedKey,json", "utf8")
        let wallet = new ethers.Wallet.fromEncryptedJsonSync(
          encryptedJson,
@@ -21,21 +21,7 @@ async function main() {
     const contract = await contractFactory.deploy()
     const deploymentReceipt = await contract.deployTransaction.wait(1) // Waits 1 block
     console.log(`Contract deployed to ${contract.address}`)
-    /* const _nonce = await wallet.getTransactionCount();
-       const tx = {
-       nonce: _nonce,
-       data: <key>,
-       value: 0,
-       gasPrice: 20000000000,
-       gasLimit: 1000000,
-       to: null,
-       chainId: 5777,
-       };
-       const sentTxResponse = wallet.sendTransaction(tx)
-       await sentTxResponse.wait(1)
-       console.log(sentTxResponse) */
-    // Gets number: (0 as it is not initialized)
-    const currentFavouriteNumber = await contract.retrieve()
+    const currentFavouriteNumber = await contract.retrieve() // Gets number: (0 as it is not initialized)
     console.log(
         `Current favourite number: ${currentFavouriteNumber.toString()}`
     )
