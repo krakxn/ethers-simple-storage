4dd9ee72ac6d991ff98b919cb1b8c577d44e6b6c
AN <108299225+krakxn@users.noreply.github.com>
2022-07-21 23:56:09 +0530

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,31 +1,30 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0; // Version should be 0.8.0 and above
+pragma solidity ^0.8.0; /// Version should be 0.8.0 and above
 
 contract SimpleStorage {
     uint256 favoriteNumber;
     
-    // Creates People object
+    /// Creates People object
     struct People {
         uint256 favoriteNumber;
         string name;
     }
 
-    People[] public people; // Array of People objects
+    People[] public people; /// Array of People objects
     
-    // Dictionary nameToFavouriteNumber with string as key
+    /// Dictionary nameToFavoriteNumber with string as key
     mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
-
-    function retrieve() public view returns (uint256) {
+    
+    function retrieve() public view returns (uint256) { /// Returns uint256 type
         return favoriteNumber;
     }
 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        people.push(People(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
+        people.push(People(_favoriteNumber, _name)); /// Pushes People objects in array people
+        nameToFavoriteNumber[_name] = _favoriteNumber; /// _name key in the dictionary now points to _favoriteNumber
     }
 }
