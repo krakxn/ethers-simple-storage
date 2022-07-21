3b84a81d239ffa65369c09ccefd7ac83b8d4dba5
AN <108299225+krakxn@users.noreply.github.com>
2022-07-21 23:58:19 +0530

    Update encryptKey.js

--- encryptKey.js
+++ encryptKey.js
@@ -1,11 +1,10 @@
-// Used to encrypt private key
 const ethers = require("ethers");
 const fs = require("fs-extra");
 
 require("dotenv").config(); // To process .env file
 async function main() {
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
-  const encryptedJsonKey = await wallet.encrypt(
+  const encryptedJsonKey = await wallet.encrypt( // For encryption
     process.env.PRIVATE_KEY_PASSWORD,
     process.env.PRIVATE_KEY
   );
